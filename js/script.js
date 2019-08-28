94b0f07f82b22604828cfc33403be091a95b4e9f
Samma <46258134+Samunter@users.noreply.github.com>
2019-08-28 16:30:08 -0700

    add comments and tidy code

--- js/script.js
+++ js/script.js
@@ -3,42 +3,15 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
+//create global variables
 const listItems = document.querySelectorAll('.student-item');
 const itemsPerPage = 10;
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
+//create function to dynamically display and hide students
 function showPage(list, page) {
   const startIndex = page * itemsPerPage - itemsPerPage;
   const endIndex = page * itemsPerPage;
-  console.log('startIndex', startIndex, 'endIndex', endIndex);
-
+  //default display students within index range, hide those outside of that range
   for (let i = 0; i < list.length; i++) {
     if (i >= startIndex && i < endIndex) {
       list[i].style.display = 'block';
@@ -48,49 +21,46 @@ function showPage(list, page) {
   }
 }
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
+//create functional page links
 function appendPageLinks(list) {
   const numberOfPages = Math.ceil(list.length / itemsPerPage);
-  console.log('Number of pages', numberOfPages);
-
+  //create and append html elements
   const div = document.createElement('div');
   div.className = 'pagination';
-  console.log('div', div);
   document.querySelector('.page').appendChild(div);
   const ul = document.createElement('ul');
   div.appendChild(ul);
+  //creates a li containing a link for every page of students
   for (let i = 1; i <= numberOfPages; i++) {
     const li = document.createElement('li');
     ul.appendChild(li);
     const a = document.createElement('a');
     a.innerHTML = i;
     if (a.innerHTML === '1') {
+      //'active' applies css styling
       a.className = 'active';
     }
     a.href = '#';
     li.appendChild(a);
   }
+  //upon clicking a link, the showPage() function runs. Page number is retrieved from the event target
   ul.addEventListener('click', e => {
-    console.log('target', e.target);
     if (e.target.tagName == 'A') {
       const links = document.querySelectorAll('a');
-      console.log('links', links);
+      const pageNumber = parseInt(e.target.innerHTML);
+      //remove 'active' class from all links
       for (let i = 0; i < numberOfPages; i++) {
         links[i].className = '';
       }
-      const pageNumber = parseInt(e.target.innerHTML);
-      console.log('page number', pageNumber);
+      //assign 'active' class to event target
       e.target.className = 'active';
       showPage(listItems, pageNumber);
     }
   });
 }
 
+//Initialization functions
+//create functional page links based on the number of students in the given list
 appendPageLinks(listItems);
+//load first page
 showPage(listItems, 1);
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
